29d84022da6a55cd2d80989a2871f6a28a5aff9b
Zubair Ahmed Khushk <70325002+Zubair-12@users.noreply.github.com>
2020-12-20 19:11:12 +0500

    script.js

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
   document.getElementById("body-content").style.marginLeft = "0";
 }
- function f(t,e,n,i,s){for(var o,a=[],r=0,l=t.length,c=null!=e;l>r;r++)(o=t[r])&&(n&&!n(o,i,s)||(a.push(o),c&&e.push(r))) return a}
+
 
 
